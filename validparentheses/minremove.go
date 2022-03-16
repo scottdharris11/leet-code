2f2b7c0ae38f8e80141d377c9e8df7afba4e8651
scottdharris <scottdharris11@yahoo.com>
2022-03-16 08:12:56 -0500

    clean code and add comments

--- validparentheses/minremove.go
+++ validparentheses/minremove.go
@@ -8,23 +8,24 @@ func minRemoveToMakeValid(s string) string {
 	for _, c := range s {
 		switch c {
 		case ')':
+			// skip character if not matching begin paren on stack
 			n := stackLen - 1
 			if n < 0 {
 				continue
 			}
 			stack = stack[:n]
 			stackLen--
-			fallthrough
-		default:
-			if c == '(' {
-				stack = append(stack, oIdx)
-				stackLen++
-			}
-			o[oIdx] = c
-			oIdx++
+		case '(':
+			// capture index of begin paren on stack
+			stack = append(stack, oIdx)
+			stackLen++
 		}
+		o[oIdx] = c
+		oIdx++
 	}
 
+	// splice out any beginning parens that were not closed
+	// start at back so index values remain valid
 	for stackLen > 0 {
 		i := stack[stackLen-1]
 		o = append(o[:i], o[i+1:]...)
